b6b391987af7b0ceb7e725f4293bce8857ec2504
lcaceres192 <lcaceres@barcelo.edu.ar>
2021-04-03 22:31:30 -0300

    Index.html: Se corrigieron ID de elementos. main.js: Obtiene value de elementos por ID y genera pdf.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,61 @@
+let orden;
+let ffecha;
+let nEvento;
+let areaSolicitante;
+let nombreSolicitante;
+let sedeEvento;
+
+
+/* Genrador de numero de Orden */
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
 let random = getRndInteger(100, 999);
 document.getElementById("nOrden").value = random;
+/* Genrador de numero de Orden */
 
+
+/* Fecha */
+/* Genera Fecha */
 let date = new Date();
 let year = date.getFullYear();
 let month = String(date.getMonth() + 1).padStart(2, '0');
 let todayDate = String(date.getDate()).padStart(2, '0');
 let datePattern = year + '-' + month + '-' + todayDate;
-
+/* Escribe fecha en input id=Fecha */
 document.getElementById("fecha").value = datePattern;
+/* document.write(datePattern); */
+/* Fecha */
 
-document.write(datePattern);
+
+function crearPDF() {
+    orden = document.getElementById("nOrden").value
+    ffecha = document.getElementById("fecha").value
+    nEvento = document.getElementById("nombreEvento").value
+    areaSolicitante = document.getElementById("area").value
+    nombreSolicitante = document.getElementById("nSolicitante").value
+    sedeEvento = document.getElementById("sede").value
+
+    var docDefinition = {
+        content: [`Numero de orden: ${orden}`,
+        `Fecha: ${ffecha}`,
+        `Nombre del Evento: ${nEvento}`,
+        `Area: ${areaSolicitante}`,
+        `Solicitante: ${nombreSolicitante}`,
+        `Sede: ${sedeEvento}`,
+
+        ]
+    };
+
+
+
+    /* docDefinition.content = `Numero de orden: ${orden}`; */
+
+    pdfMake.createPdf(docDefinition).download('name.pdf');
+
+}
+
+var docDefinition = { content: `Numero de orden: ${orden}` }; /* `Numero de orden: ${varia}`,  content: [ ] */
+
+docDefinition.content = `Numero de orden: ${orden}`;
